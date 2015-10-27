9f672113a7b7c04a01b6f38de38aa4723937a5cd
giwa <ugw.gi.world@gmail.com>
2015-10-27 20:37:00 +0100

    add gunicorn conf

---
+++ conf/gunicorn.py
@@ -0,0 +1,23 @@
+import multiprocessing
+
+# Server Socket
+bind = '127.0.0.1:8000'
+backlog = 2048
+
+# Worker Processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+max_requests = 0
+timeout = 30
+keepalive = 2
+debug = False
+spew = False
+
+# Logging
+logfile = '/home/ken/log/gunicorn/task_server_gunicon.log'
+loglevel = 'info'
+logconfig = None
+
+# Process Name
+proc_name = 'task_server_gunicorn'
