9e96a8b3e2dc926c790d4948c8807c70975d83d0
giwa <ugw.gi.world@gmail.com>
2015-11-03 05:21:16 +0100

    fix endpoint

--- server/api/visits.py
+++ server/api/visits.py
@@ -7,14 +7,14 @@ cxt = di.cxt
 visit_bp = Blueprint('visit_api', __name__)
 
 
-@visit_bp.route("", ["GET"])
+@visit_bp.route("", methods=["GET"])
 def get_visits():
     visits = cxt.dao.visit.get_list()
     rs = cxt.scheme.visits.dump(visits).data
     return mjsonify(rs)
 
 
-@visit_bp.route("/<int:id>", ["GET"])
+@visit_bp.route("/<int:id>", methods=["GET"])
 def get_visit(id):
     visit = cxt.dao.visit.get_by_id(id)
     rs = cxt.scheme.visit.dump(visit).data
